b38c477e86ec8ba779296a7cc3412552a54f80d1
Drachenbauer <47259059+Drachenbauer@users.noreply.github.com>
2022-11-21 19:09:22 +0100

    Added parent nodes for different stacks

---
+++ Globals.gd
@@ -0,0 +1,10 @@
+extends Node
+
+
+# Declare member variables here:
+var storage_stack = []
+var flip_open_stack = []
+var target_stacks = {"h" : [], "d" : [], "s" : [], "c" : []}
+var bottom_stacks = [[], [], [], [], [], [], []]
+var is_card_flipped = false
+var is_card_sliding = false
