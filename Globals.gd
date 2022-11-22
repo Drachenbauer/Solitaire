9366c644276f551424c35f85f46fb6f6345e0c82
Drachenbauer <47259059+Drachenbauer@users.noreply.github.com>
2022-11-23 00:15:09 +0100

    Added and modified deck-textures

--- Globals.gd
+++ Globals.gd
@@ -2,6 +2,9 @@ extends Node
 
 
 # Declare member variables here:
+const SUITS = {"h" : 0, "d" : 2, "s" : 4, "c" : 6}
+const VALUES = ["a", 2, 3, 4, 5, 6, 7, 8, 9, 10, "j", "q", "k"]
+
 var storage_stack = []
 var flip_open_stack = []
 var target_stacks = {"h" : [], "d" : [], "s" : [], "c" : []}
