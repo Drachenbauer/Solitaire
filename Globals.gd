43ec96fa4ce62eba80fedd57753e06436546267f
Drachenbauer <47259059+Drachenbauer@users.noreply.github.com>
2022-11-28 19:01:20 +0100

    bugs fixed

--- Globals.gd
+++ Globals.gd
@@ -9,6 +9,6 @@ var storage_stack = []
 var flip_open_stack = []
 var target_stacks = {"h" : [], "d" : [], "s" : [], "c" : []}
 var bottom_stacks = [[], [], [], [], [], [], []]
-var is_card_flipped = false
+var is_holding_card = false
 var is_card_sliding = false
 var flip_id
