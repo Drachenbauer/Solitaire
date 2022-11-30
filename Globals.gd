7c18b4ea31a5939d926bdd3653a5a8f2d95dc87b
Drachenbauer <47259059+Drachenbauer@users.noreply.github.com>
2022-11-30 23:24:24 +0100

    added solve check

--- Globals.gd
+++ Globals.gd
@@ -12,3 +12,13 @@ var bottom_stacks = [[], [], [], [], [], [], []]
 var is_holding_card = false
 var is_card_sliding = false
 var flip_id
+
+
+func is_solved():
+	var num_cards_on_targets = 0
+	
+	for stack_id in G.target_stacks:
+		var stack = G.target_stacks[stack_id]
+		num_cards_on_targets += stack.size()
+	
+	return num_cards_on_targets == 52
